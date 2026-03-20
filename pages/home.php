c3320c87d6b9d5d417463f396da7b57339d2da54
Daniil <daniillishankov@gmail.com>
2026-03-20 09:42:56 +0100

    fghjk

--- pages/home.php
+++ pages/home.php
@@ -4,16 +4,16 @@
             <div class="advertorial">
                 <h2>Hét platform om een auto te huren</h2>
                 <p>Snel en eenvoudig een auto huren. Natuurlijk voor een lage prijs.</p>
-                <a href="#" class="button-primary">Huur nu een auto</a>
-                <img src="assets/images/car-rent-header-image-1.png" alt="">
-                <img src="assets/images/header-circle-background.svg" alt="" class="background-header-element">
+                <a href="/car-detail" class="button-primary">Huur nu een auto</a>
+                <img src="/assets/images/car-rent-header-image-1.png" alt="">
+                <img src="/assets/images/header-circle-background.svg" alt="" class="background-header-element">    //class don't let to click the button
             </div>
             <div class="advertorial">
                 <h2>Wij verhuren ook bedrijfswagens</h2>
                 <p>Voor een vaste lage prijs met prettig voordelen.</p>
-                <a href="#" class="button-primary">Huur een bedrijfswagen</a>
-                <img src="assets/images/car-rent-header-image-2.png" alt="">
-                <img src="assets/images/header-block-background.svg" alt="" class="background-header-element">
+                <a href="/car-detail" class="button-primary">Huur een bedrijfswagen</a>
+                <img src="/assets/images/car-rent-header-image-2.png" alt="">
+                <img src="/assets/images/header-block-background.svg" alt="" class="background-header-element">     //class don't let to click the button
 
             </div>
         </div>
@@ -32,9 +32,9 @@
                 </div>
                 <img src="assets/images/products/car%20(<?= $i ?>).svg" alt="">
                 <div class="car-specification">
-                    <span><img src="assets/images/icons/gas-station.svg" alt="">90l</span>
-                    <span><img src="assets/images/icons/car.svg" alt="">Schakel</span>
-                    <span><img src="assets/images/icons/profile-2user.svg" alt="">2 Personen</span>
+                    <span><img src="/assets/images/icons/gas-station.svg" alt="">90l</span>
+                    <span><img src="/assets/images/icons/car.svg" alt="">Schakel</span>
+                    <span><img src="/assets/images/icons/profile-2user.svg" alt="">2 Personen</span>
                 </div>
                 <div class="rent-details">
                     <span><span class="font-weight-bold">€249,00</span> / dag</span>
@@ -55,9 +55,9 @@
                 </div>
                 <img src="assets/images/products/car%20(<?= $i ?>).svg" alt="">
                 <div class="car-specification">
-                    <span><img src="assets/images/icons/gas-station.svg" alt="">90l</span>
-                    <span><img src="assets/images/icons/car.svg" alt="">Schakel</span>
-                    <span><img src="assets/images/icons/profile-2user.svg" alt="">2 People</span>
+                    <span><img src="/assets/images/icons/gas-station.svg" alt="">90l</span>
+                    <span><img src="/assets/images/icons/car.svg" alt="">Schakel</span>
+                    <span><img src="/assets/images/icons/profile-2user.svg" alt="">2 People</span>
                 </div>
                 <div class="rent-details">
                     <span><span class="font-weight-bold">€249,00</span> / dag</span>
@@ -67,7 +67,7 @@
         <?php endfor; ?>
     </div>
     <div class="show-more">
-        <a class="button-primary" href="#">Toon alle</a>
+        <a class="button-primary" href="/ons-aanbod">Toon alle</a>
     </div>
     </main>
 
