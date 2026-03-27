28014d0a0fc297537a14fbd5ae95f16add3710d1
Hugo-Grandia-my-guy <hugodepdep6@gmail.com>
2026-03-27 11:32:01 +0100

    commit de over-ons

--- pages/over-ons.php
+++ pages/over-ons.php
@@ -14,7 +14,7 @@
         </div>
     </div>
     <div class="team">
-        <h2>Ons team</h2>
+        <h2><i>Ons Team:</i></h2>
 
         <div class="team-grid">
 
@@ -22,28 +22,39 @@
                 <img src="assets/images/team/brian-mensah.WEBP" alt="Bryan Mensah, CEO van Rydr" width="200">
                 <h3>Bryan Mensah</h3>
                 <p><strong>CEO</strong></p>
-                <p>Stuurt de visie van Rydr en bouwt aan de toekomst van mobiliteit.</p>
+                <p>Stuurt de visie van Rydr en bouwt aan de toekomst van mobiliteit.<br>
+                een cruciale rol in de ondersteuning van verschillende bedrijfsfuncties, variërend van HR tot administratie.<br>
+                Ze zorgen ervoor dat interne processen efficiënt verlopen en bieden de benodigde ondersteuning aan medewerkers om hun taken uit te voeren.</p>
             </div>
 
             <div class="member">
                 <img src="/assets/images/team/jasper-van-den-brink.WEBP" alt="Jasper van den Brink, CTO van Rydr" width="200">
                 <h3>Jasper van den Brink</h3>
                 <p><strong>CTO</strong></p>
-                <p>Leidt de technologische ontwikkeling en zorgt voor schaalbaarheid van het platform.</p>
+                <p>Leidt de technologische ontwikkeling en zorgt voor schaalbaarheid van het platform.<br>
+                    de Data & Analytist bij Rydr is als een krachtcentrale van slimme data-expert is een persoon met een diepgaande passie voor de zakelijke waarde van data.<br>
+                    Voor onze klanten zorgen ze ervoor dat mobiel bankieren en dat betalingen soepel verlopen. het belangrijkste is dat hun gegevens altijd veilig zijn.<br>
+                    Met een scherp oog voor detail en een analytische geest, transformeert deze Data & Analytist complexe gegevens in waardevolle inzichten die de groei van Rydr stimuleren.</p>
             </div>
 
             <div class="member">
                 <img src="/assets/images/team/lotte-de-graaf.WEBP" alt="Lotte de Graaf, Lead Designer bij Rydr" width="200">
                 <h3>Lotte de Graaf</h3>
                 <p><strong>Lead Designer</strong></p>
-                <p>Ontwerpt intuïtieve en moderne gebruikerservaringen.</p>
+                <p>Ontwerpt intuïtieve en moderne gebruikerservaringen.<br>
+                De Marketingafdeling bij Rydr is als de spil in een dynamische machine.<br>
+                Haar taak? Zorgen dat Rydr online gezien wordt en klanten aantrekt.<br>
+                Dit betekent digitale marketingstrategieën bedenken, online kanalen beheren, de merkidentiteit bewaken en onze producten in de spotlight zetten.<br>
+                Ze houden zich bezig met het opstellen van risicobeleid, zorgen dat we aan alle regels voldoen en houden een oogje op de operationele risico's </p>
             </div>
 
             <div class="member">
                 <img src="/assets/images/team/youssef-amrani.WEBP" alt="Youssef Amrani, Marketing Manager bij Rydr" width="200">
                 <h3>Youssef Amrani</h3>
                 <p><strong>Marketing Manager</strong></p>
-                <p>Verbindt het merk Rydr met klanten en de markt.</p>
+                <p>Verbindt het merk Rydr met klanten en de markt.<br>
+                De Marketingafdeling bij Rydr is als de spil in een dynamische machine<br>
+                Ze zetten alles op alles zodat klanten hun geldzaken soepel en veilig kunnen regelen.</p>
             </div>
 
         </div>
