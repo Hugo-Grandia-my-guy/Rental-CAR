13bbffb50b25a096b95a584303a34ee7c886401e
Daniil <daniillishankov@gmail.com>
2026-04-02 14:24:46 +0200

    filter :)
    Yeeeaaahhhhh, maar alles is op één colomn :(

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@ $userId = $_SESSION['id'] ?? null;
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Rydr</title>
     <link rel="stylesheet" href="/assets/css/main.css">
-    <link rel="icon" type="image/png" href="/assets/images/favicon.ico" sizes="32x32">
+    <link rel="icon" type="image/png" href="/assets/images/rental-icon.png" sizes="32x32">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
